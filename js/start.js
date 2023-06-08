d57d0baf3a8b86ca92ff36816ae56e8926be93df
Ta1Vos <302196386@student.rocmondriaan.nl>
2023-06-08 13:24:58 +0200

    fixed age verification bug

--- js/start.js
+++ js/start.js
@@ -3,23 +3,22 @@ function redirectToHome() {
     window.location = "home.html";
 }
 
+//Age restriction before page loads 
+function ageVerification(age) {
+    if (age >= 18 && age <= 100) {
+        redirectToHome();
+    } else if (age < 18) {
+        let textPlace = document.querySelector(`.age-not-verified`);
+        textPlace.innerHTML += (`<h1>Helaas is deze website alleen toegankelijk voor 18+ jarigen, excuses voor het ongemak.</h1>`);
+    } else if (age > 100) {
+        alert(`Geef een realistisch getal op a.u.b.`);
+        ageVerification();
+    } else {
+        alert(`Geef een getal op, uw huigige invoer is incorrect.`);
+        ageVerification(prompt(`Wat is uw leeftijd? Geef a.u.b. een getal op`));
+    }
+}
 
 function init() {
-    //Age restriction before page loads
-    function ageVerification(age) {
-        if (age >= 18 && age <= 100) {
-            redirectToHome();
-        } else if (age < 18) {
-            let textPlace = document.querySelector(`.age-not-verified`);
-            textPlace.innerHTML += (`<h1>Helaas is deze website alleen toegankelijk voor 18+ jarigen, excuses voor het ongemak.</h1>`);
-        } else if (age > 100) {
-            alert(`Geef een realistisch getal op a.u.b.`);
-            ageVerification();
-        } else {
-            alert(`Geef een getal op, uw huigige invoer is incorrect.`);
-            ageVerification();
-        }
-    }
-
     ageVerification(prompt(`Wat is uw leeftijd? Geef a.u.b. een getal op`));
 }
